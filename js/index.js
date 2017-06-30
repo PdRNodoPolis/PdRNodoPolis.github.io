7cd73954e53a05794254ce0269f310996672bac5
pferder <pferder@gmail.com>
2017-06-29 21:54:05 -0300

    diseño, fix del sidebar, falta revisar textos, avances con form (muy poquitou)

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
 $(document).foundation();
+
+var sendProposal = function() {
+    var name = $('#issue-name').val();
+    var email = $('#issue-email').val();
+    var msg = $('#issue-msg').val();
+    var date = new Date().getDate();
+    var month = new Date().getMonth();
+    var year = new Date().getFullYear();
+    var now = date +'/'+month+'/'+year;
+    var data = {
+        title: name + ' - ' + email,
+        body: now + ' - ' + msg,
+    };
+    console.log(data);
+    
+}
