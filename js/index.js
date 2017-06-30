1a342499d4f978150d419b8ea105c64dcc3c1eba
pferder <pferder@gmail.com>
2017-06-30 02:38:32 -0300

    mailer

--- js/index.js
+++ js/index.js
@@ -1,17 +1,27 @@
 $(document).foundation();
 
 var sendProposal = function() {
+    var sendingMessage = true;
     var name = $('#issue-name').val();
     var email = $('#issue-email').val();
     var msg = $('#issue-msg').val();
     var date = new Date().getDate();
     var month = new Date().getMonth();
     var year = new Date().getFullYear();
-    var now = date +'/'+month+'/'+year;
+    var now = date + '/' + month + '/' + year;
     var data = {
-        title: name + ' - ' + email,
+        subject: name + ' - ' + email,
         body: now + ' - ' + msg,
     };
     console.log(data);
-    
+
+
+    $.ajax({
+    type: "POST",
+    url: "/mailer/email.php",
+    data: data,
+    success: function(){
+        alert('Mensaje enviado correctamente.')
+    }
+});
 }
